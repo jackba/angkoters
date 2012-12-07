dc1e366d73ba243d18a2cea949de6a3634172591
hamim.email <hamim.email@gmail.com>
2012-12-07 15:30:44 +0000

    edit class datamanager

--- angkoters/src/com.angkoters/DatabaseManager.java
+++ angkoters/src/com.angkoters/DatabaseManager.java
@@ -5,28 +5,45 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class dbManager extends SQLiteOpenHelper{
-	private static String namaDB = "angkotersDB";
-	private String sql = "CREATE TABLE daftarRuteTB(_id INTEGER PRIMARY KEY AUTOINCREMENT , kodeAngkot TEXT, ruteAngkot TEXT)";
-	private String query = "SELECT idRute, kodeAngkot, ruteAngkot";
-	public dbManager(Context context) {
-		super(context, namaDB , null,1);
-		// TODO Auto-generated constructor stub
-	}
-	
-	public void onCreate(SQLiteDatabase db){
-    db.execSQL("DROP TABLE IF EXISTS daftarRuteTB");
-		db.execSQL(sql);
-	}
+public class DatabaseManager{
+	private static String namaDB = "AngkotersDB";
+  private static final String namaTabel = "DataAngkot";
+  private static final String ID = "_id";
+  private static final String KODE_ANGKOT = "kodeangkot";
+  private static final String RUTE_ANGKOT = "ruteangkot";
+  private static final String BIAYA_ANGKOT = "biayaangkot";
+  private static final String WAKTU_OPERASI = "waktuoperasi";
+	private String sql = "CREATE TABLE " + DataAngkot + "("+
+   ID +" INTEGER PRIMARY KEY AUTOINCREMENT , "+
+   KODE_ANGKOT+" TEXT, "+
+   RUTE_ANGKOT+" TEXT, "+
+   BIAYA_ANGKOT+" INTEGER, "+
+   WAKTU_OPERASI+" TEXT)";
 
-	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
-		
+  private Context context;
+  private SQLiteDatabase db;
+  private DatabaseOpenHelper dbHelper;
+   
+	public DatabaseManager(Context ctx) {
+		this.context = ctx;
+    dbHelper = new DataseOpenHelper(context);
+    db = dbHelper.getWritableDatabase();
 	}
+   
+  private static class DatabaseOpenHelper extends SQLiteDatabase{
+      public void onCreate(SQLiteDatabase db){
+        db.execSQL("DROP TABLE IF EXISTS namaTabel");
+        db.execSQL(sql);
+     }
+
+       @Override
+       public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		
+     }
+  }	
 	
 	public Cursor getDataAngkot(){
-		return(getReadableDatabase().rawQuery(query,null));
+		return(getReadableDatabase().rawQuery("SELECT * FROM namaTabel",null));
 	}
 	
 	public String getIdAngkot(Cursor cur){
@@ -40,5 +57,31 @@ public class dbManager extends SQLiteOpenHelper{
 	public String getRuteAngkot(Cursor cur){
 		return cur.getString(2);
 	}
+   
+     public ArrayList<ArrayList<Object>> ambilSemuaBaris(){
+     ArrayList<ArrayList<Object>> dataArray = new ArrayList<ArrayList<Object>>();
+     Cursor cur;
+     
+    try
+    {
+      cur = db.query(namaTabel, new String[]{ID, KODE_ANGKOT, RUTE_ANGKOT,BIAYA_ANGKOT, WAKTU_OPERASI},null,null,null,null,null);
+      if(!cur.moveToFirst()){
+        do{
+        ArrayList<Object> dataList = ArrayList<Object>();
+        dataList.add(cur.getLong(0));
+        dataList.add(cur.getLong(1));
+        dataList.add(cur.getLong(2));
+        dataList.add(cur.getLong(3));
+        dataList.add(cur.getLong(4));
+        dataArray.add(dataList);
+        } while (cur.moveToNext());
+        }
+      
+    } catch(Exception e){
+      e.printStackTrace();
+      log.e("Database Error", e.toString());
+    }
+    return dataArray;
+  }
 
 }
