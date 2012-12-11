581543f6d29da6e6eb399811c1f865bf10659e81
hamim.email <hamim.email@gmail.com>
2012-12-11 09:45:57 +0000

    edit DBM

--- angkoters/src/com.angkoters/DatabaseManager.java
+++ angkoters/src/com.angkoters/DatabaseManager.java
@@ -1,7 +1,6 @@
 package com.angkoters.project;
 
 import android.content.Context;
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
