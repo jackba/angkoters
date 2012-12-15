087e27c8e3d2622639373b5313fa44c21b0a3f61
hamim.email <hamim.email@gmail.com>
2012-12-15 18:32:51 +0000

    Class untuk kontroler detail angkot pilihan user

---
+++ angkoters/src/com.angkoters/DetailAngkot.java
@@ -0,0 +1,41 @@
+package com.angkoters;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+
+public class DetailAngkot extends Activity{
+	
+	DatabaseManager dm;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.viewdaftarangkot);
+ 
+		dm = new DatabaseManager(this);
+		viewDetailAngkot();
+	}
+	
+	private String ambilParameter(){
+		String KodeAngkot;
+		Bundle bundle = getIntent().getExtras();
+		KodeAngkot = bundle.getString("KodePilihan");
+		return KodeAngkot;
+	}
+	
+	private void viewDetailAngkot(){
+		ambilParameter();
+		Cursor cur = dm.getDataRuteAngkot(ambilParameter());
+		
+		String kolomKanan [] = new String[]{
+			dm.ID, dm.KODE_ANGKOT, dm.RUTE_ANGKOT, dm.BIAYA_ANGKOT, dm.JAM_MULAI,
+			dm.JAM_SELESAI
+		};
+		
+		int kolomKiri [] = new int[]{
+				
+		};
+	}
+
+}
